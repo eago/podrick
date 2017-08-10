d63b6b08abcb04f1fd6b84fe820c3e9a24afbcbc
sihai <hhusihai@gmail.com>
2017-08-10 17:26:50 +0200

    add module leaflet

--- brienne/src/app/westeros/westeros.component.ts
+++ brienne/src/app/westeros/westeros.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as L from 'leaflet'
+
 @Component({
   selector: 'app-westeros',
   templateUrl: './westeros.component.html',
@@ -10,6 +12,12 @@ export class WesterosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    var westerosMap = L.map('westerosMap').setView([51.505, -0.09], 13);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(westerosMap);
   }
 
 }
