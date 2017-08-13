4f07c4d52ac3cbe811f6776fb7b09e7f6745f22d
sihai <hhusihai@gmail.com>
2017-08-13 17:02:59 +0200

    corrige observable bug

--- brienne/src/app/weatherComp/weather-service.service.ts
+++ brienne/src/app/weatherComp/weather-service.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 
 
 import {Http, Response} from '@angular/http'
-import { Observable} from 'rxjs/Observable'
+/*import { Observable} from 'rxjs/Observable'*/
+import { Observable } from 'rxjs';
+import 'rxjs/Rx';
 
 @Injectable()
 export class WeatherServiceService {
