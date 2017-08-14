212fed713d6ff369f66539c83758f0c472443747
sihai <hhusihai@gmail.com>
2017-08-14 11:47:14 +0200

    map got works

--- brienne/src/app/westeros/westeros.component.ts
+++ brienne/src/app/westeros/westeros.component.ts
@@ -13,10 +13,20 @@ export class WesterosComponent implements OnInit {
 
   ngOnInit() {
 
-    var westerosMap = L.map('westerosMap').setView([51.505, -0.09], 13);
+/*    var westerosMap = L.map('westerosMap').setView([51.505, -0.09], 13);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(westerosMap);*/
+  
+  var westerosMap = L.map('westerosMap').setView([0,0],3);
+
+  L.tileLayer('../../assets/GOT/{z}/{x}/{y}.png', {
+      attribution: 'Game of Thrones',
+      maxZoom: 5,
+      minZoom: 1,
+      noWrap: true,
+      crs: L.CRS.Simple
     }).addTo(westerosMap);
   }
 
